670cb5b81871cef67c8d67620c0bac97baefb884
Gaogogo <cuggsh@163.com>
2018-06-19 20:29:46 +0800

    update train_test.py

--- train_test.py
+++ train_test.py
@@ -2,6 +2,7 @@ import threading
 import time
 import socket
 from configparser import ConfigParser
+import mpsched
 
 
 class io_thread(threading.Thread):
@@ -27,6 +28,31 @@ class io_thread(threading.Thread):
         fp.close()
 
 
+class record(object):
+    """docstring for record."""
+    def __init__(self, timestep=0.2, datafile="record"):
+        self.data = []
+        self.timestep = timestep
+        self.datafile = datafile
+
+    def save(self):
+        lenth = len(self.data)
+        with open(self.datafile, 'w') as f:
+            f.write(str(self.timestep))
+            f.write('\n')
+            f.write(str(lenth))
+            f.write('\n')
+            for i in range(lenth):
+                f.write('%d %d\n' % (self.data[i][0][1], self.data[i][1][1]))
+        f.close()
+
+    def put(self, recd):
+        self.data.append(recd)
+
+    def draw(self):
+        pass
+
+
 def main():
     cfg = ConfigParser()
     cfg.read('config.ini')
@@ -35,15 +61,27 @@ def main():
     PORT = cfg.getint('server', 'port')
     FILE = cfg.get('file', 'file')
     SIZE = cfg.getint('env', 'buffer_size')
+    timestep = cfg.getfloat('env', 'time')
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((IP, PORT))
     fd = sock.fileno()
-    io = io_thread(sock=sock, filename="./64mb.dat", buffer_size=SIZE)
+
+    io = io_thread(sock=sock, filename=FILE, buffer_size=SIZE)
+    r = record(timestep=timestep, datafile="record")
 
     start_time = time.time()
     io.start()
-    io.join()
+
+    while True:
+        time.sleep(timestep)
+        data = mpsched.get_sub_info(fd)
+        if len(data) == 0:
+            io.join()
+            break
+        r.put(data)
+
+    r.save()
     end_time = time.time()
     print("completion time: ", end_time - start_time)
 
