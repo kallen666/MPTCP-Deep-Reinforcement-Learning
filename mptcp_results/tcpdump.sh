ff6f3321f59678aaf0066eb41f91f526c78c62c9
ZhangHan <463808243@qq.com>
2018-06-19 20:50:36 +0800

    update

--- mptcp_results/tcpdump.sh
+++ mptcp_results/tcpdump.sh
@@ -1,2 +1,3 @@
-sudo tcpdump -i wlp3s0 -w w0.pcap &
-sudo tcpdump -i wlx485d601fa31c -w w1.pcap &
+sudo tcpdump -i wlp3s0 -w wifi1.pcap &
+sudo tcpdump -i wlx485d601fa31c -w wifi2.pcap &
+sudo tcpdump -i enp2s0 -w eth.pcap &
