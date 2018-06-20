22bbf540ac7dd50394fe2c8ecef3b910eb218903
Gaogogo <cuggsh@163.com>
2018-06-20 17:11:58 +0800

    update tc.sh

--- tc.sh
+++ tc.sh
@@ -3,11 +3,15 @@ ETH0=enp0s31f6 #有线网卡
 WIFI=wlx485d605766e1 #无线网卡
 
 ETH_RTT=50ms
-ETH_RATE=512kbit
+ETH_RATE=7040kbit
 
-WIFI_RTT=90ms
-WIFI_RATE=1024bps
+WIFI_RTT=70ms
+WIFI_RATE=9185kbit
 
 tc qd del dev $ETH0 root
-tc qd add dev $ETH0 root handle 1:0 tbf rate $ETH_RATE latency 10ms burst 1540
+tc qd add dev $ETH0 root handle 1:0 tbf rate $ETH_RATE latency 50ms burst 1540
 tc qd add dev $ETH0 parent 1:0 handle 10:0 netem delay $ETH_RTT
+
+tc qd del dev $WIFI root
+tc qd add dev $WIFI root handle 1:0 tbf rate $WIFI_RATE latency 50ms burst 1540
+tc qd add dev $WIFI parent 1:0 handle 10:0 netem delay $WIFI_RTT
